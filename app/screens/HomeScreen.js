c7aaf757e1ada35c449e0247956d7c14e74a497a
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-30 19:36:06 -0800

    ../

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,20 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react';
 import Lottie from "lottie-react-native"
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 
 const HomeScreen = () => {
+  
   return (
     <View>
       <Lottie
-        source={require("../../assets/Animation - 1738226534865.json")}
+        source={require("../../assets/Animation - 1738226708840.json")}
         style={{
-          height: responsiveHeight(55),
-          width: responsiveWidth(110),
+          height: responsiveHeight(90),
+          width: responsiveWidth(100),
         }}
         autoPlay
         loop
       />
+      <TouchableOpacity 
+      onPress={() => navigation.navigate("OnboardingScreen")}
+        style={{
+          backgroundColor: "green",
+          height: responsiveHeight(6),
+          width: responsiveHeight(15),
+          marginTop: responsiveHeight(-32),
+          alignSelf: "center",
+          borderRadius: responsiveHeight(8),
+        }}
+      >
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: responsiveFontSize(2.4),
+            fontWeight: "bold",
+            marginTop:responsiveHeight(1)
+          }}
+        >
+          Reset
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
