0280d1b394e1155f87c54e2ccee2d91cf86f6132
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-03-19 21:03:31 -0700

    ./

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -37,7 +37,7 @@ const HomeScreen = () => {
             marginTop:responsiveHeight(1)
           }}
         >
-          Resets
+          ResetsStart
         </Text>
       </TouchableOpacity>
 
