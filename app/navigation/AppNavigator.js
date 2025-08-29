184a616e4b8a141b116f636af4de8e061539e633
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-08-29 06:37:34 -0700

    ./

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import { View, Text } from "re
+import { View, Text } from "r
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
